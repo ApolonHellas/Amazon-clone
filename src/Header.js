1863bf1ccdd717679286eaec53d45085344c70bc
ApolonHellas <kica82@gmail.com>
2020-09-11 10:36:13 +0200

    amazon-clone

---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src={'http://pngimg.com/uploads/amazon/amazon_PNG11.png'}
+        alt="amazon-logo"
+      />
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header___optionLineOne">Hello Guest</span>
+          <span className="header___optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header___optionLineOne">Return</span>
+          <span className="header___optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header___optionLineOne">Your</span>
+          <span className="header___optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionTwo header___optionBasketCount">
+            0
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAWI5lSdsd8U61gb0lvNYq2PKISjEF4AHk',
+  authDomain: 'clone-35a62.firebaseapp.com',
+  databaseURL: 'https://clone-35a62.firebaseio.com',
+  projectId: 'clone-35a62',
+  storageBucket: 'clone-35a62.appspot.com',
+  messagingSenderId: '856576628042',
+  appId: '1:856576628042:web:362e9304822610dd26ddb3',
+  measurementId: 'G-H2RY7T2GZT',
+};
