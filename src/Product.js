fa5ab81cbd902076c2bd12566ba47134f4416e91
ApolonHellas <kica82@gmail.com>
2020-09-12 22:56:09 +0200

    day2

---
+++ src/Product.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Product.css';
+
+function Product({ id, title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <p className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>
+                <span>🌟</span>
+              </p>
+            ))}
+        </p>
+      </div>
+      <img src={image} alt="" />
+      <button>Add to basket</button>
+    </div>
+  );
+}
+
+export default Product;
